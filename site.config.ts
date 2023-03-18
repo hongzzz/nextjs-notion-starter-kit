0ac5777fcd8163ff63a33662d911c1d9e0a13641
Hongzzz <hongzzz@foxmail.com>
2023-03-18 20:21:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3341fd748768489999f8b750693a1f16',
+  rootNotionPageId: 'a31aac7a907c4e66b757579effa09099',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -18,8 +18,8 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: '',
-  github: '',
-  linkedin: '',
+  github: 'hongzzz',
+  linkedin: 'hongzzz',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -50,16 +50,15 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'About',
+      pageId: '1a35250d2d034cf1938c034a5de25979'
+    },
+    {
+      title: 'Contact',
+      pageId: '3341fd748768489999f8b750693a1f16'
+    }
+  ]
 })
