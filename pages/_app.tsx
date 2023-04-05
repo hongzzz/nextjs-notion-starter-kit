ace7f911a6b63b32eee65b43eea96440769dc529
Hongzzz <hongzzz@foxmail.com>
2023-04-05 19:15:01 +0800

    feat: remove posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 // global styles shared across the entire site
 import * as React from 'react'
 import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
+// import { useRouter } from 'next/router'
 import { Analytics } from '@vercel/analytics/react';
 
-import * as Fathom from 'fathom-client'
+// import * as Fathom from 'fathom-client'
 // used for rendering equations (optional)
 import 'katex/dist/katex.min.css'
-import posthog from 'posthog-js'
+// import posthog from 'posthog-js'
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-coy.css'
 // core styles shared by all of react-notion-x (required)
@@ -22,7 +22,11 @@ import 'styles/prism-theme.css'
 
 import { bootstrap } from '@/lib/bootstrap-client'
 import {
+  // fathomConfig,
+  // fathomId,
   isServer,
+  // posthogConfig,
+  // posthogId
 } from '@/lib/config'
 
 if (!isServer) {
@@ -30,6 +34,33 @@ if (!isServer) {
 }
 
 export default function App({ Component, pageProps }: AppProps) {
+  // const router = useRouter()
+  //
+  // React.useEffect(() => {
+  //   function onRouteChangeComplete() {
+  //     if (fathomId) {
+  //       Fathom.trackPageview()
+  //     }
+  //
+  //     if (posthogId) {
+  //       posthog.capture('$pageview')
+  //     }
+  //   }
+  //
+  //   if (fathomId) {
+  //     Fathom.load(fathomId, fathomConfig)
+  //   }
+  //
+  //   if (posthogId) {
+  //     posthog.init(posthogId, posthogConfig)
+  //   }
+  //
+  //   router.events.on('routeChangeComplete', onRouteChangeComplete)
+  //
+  //   return () => {
+  //     router.events.off('routeChangeComplete', onRouteChangeComplete)
+  //   }
+  // }, [router.events])
 
   return <>
     <Component {...pageProps} />
